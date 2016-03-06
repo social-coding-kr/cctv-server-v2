d04b74249208e0137858e7ae54b16b10c993f4ad
joke111 <alta.second@gmail.com>
2016-03-06 23:01:37 +0900

    프로젝트 골격 추가

---
+++ cctv-admin/src/main/java/com/socialcoding/CctvAdminApplication.java
@@ -0,0 +1,12 @@
+package com.socialcoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CctvAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CctvAdminApplication.class, args);
+	}
+}
---
+++ cctv-api/src/main/java/com/socialcoding/CctvApiApplication.java
@@ -0,0 +1,12 @@
+package com.socialcoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CctvApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CctvApiApplication.class, args);
+	}
+}
