16888a4a345780c359ce27083f84093d1d8af83c
joke111 <alta.second@gmail.com>
2016-03-08 17:29:38 +0900

    Add core project

---
+++ cctv-core/src/main/java/com/example/CctvCoreApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CctvCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CctvCoreApplication.class, args);
+	}
+}
