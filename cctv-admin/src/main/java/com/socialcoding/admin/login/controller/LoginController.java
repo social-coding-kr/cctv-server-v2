58485e223760bc22d656f2c037c8b0c083d1692f
joke111 <alta.second@gmail.com>
2016-03-23 10:56:49 +0900

    구조 수정

--- cctv-admin/src/main/java/com/socialcoding/admin/login/controller/LoginController.java
+++ cctv-admin/src/main/java/com/socialcoding/admin/login/controller/LoginController.java
@@ -1,4 +1,4 @@
-package com.socialcoding.admin.dashboard.controller;
+package com.socialcoding.admin.login.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
