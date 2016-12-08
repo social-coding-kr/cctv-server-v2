ce87ef066e192682e74de407787ef1b54334c910
joke111 <alta.second@gmail.com>
2016-12-08 16:51:24 +0900

    add pid file

--- cctv-api/src/main/java/com/socialcoding/CctvApiApplication.java
+++ cctv-api/src/main/java/com/socialcoding/CctvApiApplication.java
@@ -2,12 +2,17 @@ package com.socialcoding;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.system.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class CctvApiApplication {
 
+	private static final String PID_FILE_NAME = "cctv-api-server.pid";
+
 	public static void main(String[] args) {
-		SpringApplication.run(CctvApiApplication.class, args);
+		SpringApplication application = new SpringApplication(CctvApiApplication.class);
+		application.addListeners(new ApplicationPidFileWriter(PID_FILE_NAME));
+		application.run(args);
 	}
 
 }
