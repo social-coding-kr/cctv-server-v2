b0fa51e52e5cfccce93ebf39b2a4dafbfe11988c
joke111 <alta.second@gmail.com>
2017-03-14 13:13:04 +0900

    불필요한 설정 삭제

--- cctv-api/src/main/java/com/socialcoding/config/CctvJpaRepositoryConfig.java
+++ cctv-api/src/main/java/com/socialcoding/config/CctvJpaRepositoryConfig.java
@@ -3,19 +3,13 @@ package com.socialcoding.config;
 
 import com.socialcoding.domain.CctvDomains;
 import com.socialcoding.domain.base.entity.AuditorInjector;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.data.convert.Jsr310Converters;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import javax.persistence.EntityManagerFactory;
-import javax.sql.DataSource;
 
 @Configuration
 @EnableJpaRepositories(basePackageClasses = CctvDomains.class)
@@ -23,20 +17,6 @@ import javax.sql.DataSource;
 @EnableTransactionManagement
 @EnableJpaAuditing
 public class CctvJpaRepositoryConfig {
-
-    @Autowired
-    private EntityManagerFactory entityManagerFactory;
-
-    @Autowired
-    private DataSource dataSource;
-
-    @Bean
-    public JpaTransactionManager transactionManager() {
-        JpaTransactionManager jpaTransactionManager = new JpaTransactionManager();
-        jpaTransactionManager.setEntityManagerFactory(entityManagerFactory);
-        jpaTransactionManager.setDataSource(dataSource);
-        return jpaTransactionManager;
-    }
 
     @Bean
 	public AuditorInjector auditorAware() {
