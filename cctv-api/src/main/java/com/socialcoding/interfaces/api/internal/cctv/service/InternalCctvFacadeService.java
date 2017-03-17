15e1f8305a97d584bde736c1666653036a40c585
joke111 <alta.second@gmail.com>
2017-03-17 18:52:01 +0900

    구현중

--- cctv-api/src/main/java/com/socialcoding/interfaces/api/internal/cctv/service/InternalCctvFacadeService.java
+++ cctv-api/src/main/java/com/socialcoding/interfaces/api/internal/cctv/service/InternalCctvFacadeService.java
@@ -4,6 +4,7 @@ import com.socialcoding.domain.cctv.model.OfficialCctvInsertForm;
 import com.socialcoding.domain.cctv.service.CctvFacadeService;
 import com.socialcoding.domain.common.service.CsvService;
 import com.socialcoding.interfaces.api.internal.cctv.dto.OfficialCctvRegisterFileForm;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +14,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 public class InternalCctvFacadeService {
 
@@ -28,21 +30,45 @@ public class InternalCctvFacadeService {
 
 	public void register(OfficialCctvRegisterFileForm registerForm) {
 
+		Map<Long, Map<String, String>> official = csvService.read(Paths.get(registerForm.getOfficialCctvFilePath()).toFile(), line -> {
+			Map<String, String> data = new HashMap<>();
+			data.put("cctvId", line[0]);
+			data.put("cctvName", line[1]);
+			data.put("address", line[2]);
+			data.put("borough", line[3]);
+			data.put("dong", line[4]);
+			data.put("range")
+			return data;
+		}).stream().collect(Collectors.toMap(map -> Long.parseLong(map.get("cctvId")), map -> map));
+
 		Map<Long, Map<String, String>> common = csvService.read(Paths.get(registerForm.getCommonCctvFilePath()).toFile(), line -> {
 			Map<String, String> data = new HashMap<>();
 			return data;
 		}).stream()
-			.collect(Collectors.toMap(map -> map.get("id"), map -> map));
+			.collect(Collectors.toMap(map -> Long.parseLong(map.get("cctvId")), map -> map));
 
-		Map<Long, Map<String, String>> official = csvService.read(Paths.get(registerForm.getOfficialCctvFilePath()).toFile(), line -> {
 
-		});
+		log.debug("count: {}, {}", official.size(), common.size());
 
-		mergeById(common, official).forEach(cctvFacadeService::insert);
+//		mergeById(official, common).forEach(cctvFacadeService::insert);
 	}
 
-	private List<OfficialCctvInsertForm> mergeById(Map<Long, Map<String, String>> common, Map<Long, Map<String, String>> official) {
-		return null;
+	private List<OfficialCctvInsertForm> mergeById(Map<Long, Map<String, String>> official, Map<Long, Map<String, String>> common) {
+		return official.entrySet().stream()
+			.filter(entry -> common.containsKey(entry.getKey()))
+			.map(entry -> {
+				Map<String, String> officialElements = entry.getValue();
+				Map<String, String> commonElements = common.get(entry.getKey());
+
+				Map<String, String> merged = new HashMap<>(officialElements);
+				merged.putAll(commonElements);
+				return merged;
+			})
+			.map(mergedMap -> {
+				OfficialCctvInsertForm insertForm = new OfficialCctvInsertForm();
+				return insertForm;
+			})
+			.collect(Collectors.toList());
 	}
 
 }
