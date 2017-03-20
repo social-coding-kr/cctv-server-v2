37eac620e2676eccf5dd3a27910bd4e0cbd7e531
joke111 <alta.second@gmail.com>
2017-03-20 17:38:16 +0900

    불필요한 클래스 삭제

--- cctv-api/src/main/java/com/socialcoding/domain/cctv/service/CctvCommandService.java
+++ cctv-api/src/main/java/com/socialcoding/domain/cctv/service/CctvCommandService.java
@@ -1,9 +1,7 @@
 package com.socialcoding.domain.cctv.service;
 
 import com.socialcoding.domain.cctv.entity.CctvEntity;
-import com.socialcoding.domain.cctv.entity.OfficialCctvEntity;
 import com.socialcoding.domain.cctv.repository.CctvRepository;
-import com.socialcoding.domain.cctv.repository.OfficialCctvRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,19 +11,12 @@ public class CctvCommandService {
 	private final CctvRepository cctvRepository;
 
 	@Autowired
-	private OfficialCctvRepository officialCctvRepository;
-
-	@Autowired
 	public CctvCommandService(CctvRepository cctvRepository) {
 		this.cctvRepository = cctvRepository;
 	}
 
 	public CctvEntity insert(CctvEntity cctv) {
 		return cctvRepository.save(cctv);
-	}
-
-	public OfficialCctvEntity insert(OfficialCctvEntity entity) {
-		return officialCctvRepository.save(entity);
 	}
 
 }
