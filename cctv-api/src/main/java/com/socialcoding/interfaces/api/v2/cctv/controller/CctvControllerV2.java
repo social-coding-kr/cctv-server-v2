6106790ef77d57eba82f792510b1fb51414bbfb5
joke111 <alta.second@gmail.com>
2017-03-22 17:24:43 +0900

    url 수정

--- cctv-api/src/main/java/com/socialcoding/interfaces/api/v2/cctv/controller/CctvControllerV2.java
+++ cctv-api/src/main/java/com/socialcoding/interfaces/api/v2/cctv/controller/CctvControllerV2.java
@@ -3,7 +3,9 @@ package com.socialcoding.interfaces.api.v2.cctv.controller;
 import com.socialcoding.interfaces.api.v2.cctv.dto.CctvDto;
 import com.socialcoding.interfaces.api.v2.cctv.service.CctvFacadeServiceV2;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CctvControllerV2 {
@@ -15,7 +17,7 @@ public class CctvControllerV2 {
 		this.cctvFacadeService = cctvFacadeServiceV2;
 	}
 
-	@GetMapping("/api/v2/cctv/{name}")
+	@GetMapping("/api/v2/cctvs/{name}")
 	public CctvDto get(@PathVariable String name) {
 		return cctvFacadeService.getCctv(name);
 	}
